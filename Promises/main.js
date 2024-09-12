24219a1e3bcde015455c23a94e9ea6260c2d4f2e
alexwebcoder <alexdeleon@stayup.com>
2024-09-11 20:02:50 -0700

    Execution Context Sept 11

--- Promises/main.js
+++ Promises/main.js
@@ -42,10 +42,10 @@ function renderImages() {
         return response.json()
     })
     .then(data => {
-        console.log(data)
+        // console.log(data)
     })
     .catch(error => {
-        console.log('there was a problem with the fetch operation:', error)
+        // console.log('there was a problem with the fetch operation:', error)
     })
 }
 
@@ -60,10 +60,10 @@ function renderTheImage(){
         } return response.json()
     })
     .then(data => {
-        console.log(data)
+        // console.log(data)
     })
     .catch(error => {
-        console.log('there was an error', error)
+        // console.log('there was an error', error)
     })
 }
 
@@ -78,10 +78,10 @@ function renderItAgain(){
         }return response.json()
     })
     .then(data =>{
-        console.log(data)
+        // console.log(data)
     })
     .catch(error => {
-        console.log('there was an error:', error)
+        // console.log('there was an error:', error)
     })
 }
 
@@ -93,24 +93,216 @@ async function renderTheImages(){
 }
 
 
-console.log('Start');
+// console.log('Start');
 
 setTimeout(() => {
-  console.log('Timeout 1');
+//   console.log('Timeout 1');
 }, 0);
 
 Promise.resolve().then(() => {
-  console.log('Promise 1');
+//   console.log('Promise 1');
 });
 
 setTimeout(() => {
-  console.log('Timeout 2');
+//   console.log('Timeout 2');
 }, 100);
 
 Promise.resolve().then(() => {
-  console.log('Promise 2');
+//   console.log('Promise 2');
 });
 
-console.log('End');
+// console.log('End');
 
 //Start, End, Promise 1, Promise 2, Timeout 1, Timeout 2
+
+
+function displayImagesNow(){
+    fetch('https://dog.ceo/api/breeds/image/random/5')
+    .then(response => {
+        if(!response.ok){
+            throw new Error('bad fetch')
+        }
+        return response.json();
+    })
+    .then(data => {
+        // console.log(data)
+    })
+    .catch(error => {
+        // console.log('there was an error ya see:', error)
+    })
+}
+
+displayImagesNow()
+
+// console.log('X');
+
+// setTimeout(() => {
+//   console.log('Y');
+//   Promise.resolve().then(() => {
+//     console.log('Z');
+//   });
+// }, 100);
+
+// Promise.resolve().then(() => {
+//   console.log('A');
+//   setTimeout(() => {
+//     console.log('B');
+//   }, 50);
+// });
+
+// setTimeout(() => {
+//   console.log('C');
+// }, 0);
+
+// Promise.resolve().then(() => {
+//   console.log('D');
+// });
+
+// console.log('E');
+
+//X, E, A, D, C, B, Y, Z
+
+// console.log('1');
+
+// setTimeout(() => {
+//   console.log('2');
+// }, 0);
+
+// Promise.resolve().then(() => {
+//   console.log('3');
+//   setTimeout(() => {
+//     console.log('4');
+//   }, 0);
+// });
+
+// Promise.resolve().then(() => {
+//   console.log('5');
+// });
+
+// console.log('6');
+
+// setTimeout(() => {
+//   console.log('7');
+// }, 100);
+
+// Promise.resolve().then(() => {
+//   setTimeout(() => {
+//     console.log('8');
+//   }, 50);
+// });
+
+// console.log('9');
+// 1, 6, 9, 3, 5, 2, 4, 8, 7
+
+// console.log('A');
+
+// const promise1 = new Promise((resolve) => {
+//   console.log('B');
+//   resolve('C');
+// });
+
+// const promise2 = new Promise((resolve) => {
+//   console.log('D');
+//   setTimeout(() => {
+//     resolve('E');
+//   }, 0);
+// });
+
+// promise1.then((value) => {
+//   console.log(value);
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve('F');
+//     }, 0);
+//   });
+// }).then((value) => {
+//   console.log(value);
+// });
+
+// promise2.then((value) => {
+//   console.log(value);
+//   return 'G';
+// }).then((value) => {
+//   console.log(value);
+// });
+
+// console.log('H');
+
+//A, B, D, H, C, E, G, F
+
+// console.log('A');
+
+// Promise.resolve().then(() => {
+//   console.log('B');
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve('D');
+//     }, 0);
+//   });
+// }).then((value) => {
+//   console.log('C');
+//   return value;
+// });
+
+// console.log('E');
+
+//A, E, B, C, 
+
+// console.log('One')
+
+// setTimeout(()=>{
+//     console.log('Two')
+// }, 5000)
+
+// Promise.resolve().then(function(){
+//     console.log('Three')
+// })
+
+// console.log('Four')
+
+// One, Four, Three, Two
+
+// for(var i = 0; i < 3; i++){
+//     console.log(i)
+//     setTimeout(()=>{
+//         console.log(i)
+//     }, 1000)
+// }
+
+const numbers = [1, 2, 3, 4, 5];
+
+const [a, b, c, ...d] = numbers;
+console.log(d)
+
+const duplicateKey = {a: 'one', b: 'two', a: 'three'}
+
+console.log(duplicateKey)
+
+const A = {}
+const B = {key: 'B'};
+const C = {key: 'C'};
+//object keys are automatically converted into strings
+A[B] = 123
+A[C] = 456
+
+console.log(A[B])
+
+//splice modifies original array
+//first arg is the index, second is the delete count
+//from first index, remove x amount of items
+//third index is what you want to add. you can add as many as you like
+let array = [1, 2, 3, 4, 5];
+console.log(array.splice(1, 1))
+console.log(array)
+
+let days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
+console.log(days.splice(4, 1, 'funday', 'viernes', 'any day of the week'))
+console.log(days)
+
+
+//slice keeps original array intact
+//slice as 2 arguments. starting index and ending index (non inclusive)
+let array2 = [1, 2, 3, 4, 5];
+console.log(array2.slice(2,3))
+console.log(array2)
