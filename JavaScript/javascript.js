0e92c6ff96d90ad67bd8af51b9a3e7eb7cc8ded3
alexwebcoder <alexdeleon@stayup.com>
2024-11-13 18:55:24 -0800

    laid out landing page

---
+++ JavaScript/javascript.js
@@ -0,0 +1,27 @@
+let name = 'John';
+let surname = "Doe";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log(age);
+
+const pi = 3.14;
+console.log(pi);
+
+console.log((3 + 2) - 76 * (1 + 1));
+
+let a = 10;
+console.log(9 * a);
+
+// let b = 7 * a;
+// console.log(b);
+
+let dodgerString = 'Go Dodgers Go';
+let hello = 'Hello, World!';
+console.log(hello.slice(-13, -8));
+console.log(dodgerString.substring(3));
