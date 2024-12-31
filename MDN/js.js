4952cbecaed2497aaab1c584ce88df41c0f0550a
alexwebcoder <alexdeleon@stayup.com>
2024-12-30 20:40:07 -0800

    string manipulation practice

--- MDN/js.js
+++ MDN/js.js
@@ -6,3 +6,20 @@ function reverseWords(str){
 reverseWords('Hello world, how are you?')
 
 console.log(Math.floor(Math.random() * 100))
+
+function convertSentence(str) {
+    let newStr = str.split(' ').map(item => item.split('').reverse().join('')).join(' ');
+    return newStr;
+}
+
+console.log(convertSentence('Hello world from JavaScript'))
+//olleH dlrow morf tpircSavaJ
+
+function abbreviateName(str){
+ const abbreviated = str.split(' ');
+ const lastName = abbreviated.pop()
+ const firstLetter = abbreviated[0][0] + `. `
+return firstLetter + lastName;
+}
+
+console.log(abbreviateName('Mary Ann Louise Smith'))
