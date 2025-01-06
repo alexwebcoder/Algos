99a30a6e2b8917809d386e1e3260f950b357c8a2
alexwebcoder <alexdeleon@stayup.com>
2025-01-05 20:38:55 -0800

    changed logic to include negative numbers and arrays with one item

--- MDN/ReturnValues/index.js
+++ MDN/ReturnValues/index.js
@@ -1,4 +1,5 @@
-const input = document.querySelector('.numberInput');
+(function(){
+    const input = document.querySelector('.numberInput');
 const para = document.querySelector('p');
 const br = document.createElement('br');
 para.appendChild(br);
@@ -39,46 +40,46 @@ input.addEventListener('change', ()=>{
     }
 
 });
+})();
 
-//array of numbers to be passed
-const numbers = [44, 33, 55, 77, 100, 22, 5];
 
-function findLargestNumber(array) {
+(function(){
 
-//creates a paragraph element
- const para = document.createElement('p');
- //adds a class to the paragraph element
- para.classList.add('largest-number');
- //appends the paragraph element to the page
- document.body.appendChild(para);
-
- //Throws an error if the parameter is not an array or if the function is called without an array being passed
- if(!Array.isArray(array) || arguments.length !== 1 ){
-    return para.textContent = 'You must input one array as an argument!'
-}
-
-//Throws an error if the input is an empty array
- if(array.length === 0){
-     return para.textContent = 'The array cannot be empty!'
+    //array of numbers to be passed
+    const numbers = [44, 33, 55, 77, 100, 22, 5];
+    
+    //creates a paragraph element
+     const para = document.createElement('p');
+     //adds a class to the paragraph element
+     para.classList.add('largest-number');
+     //appends the paragraph element to the page
+     document.body.appendChild(para);
+    
+    function findLargestNumber(array) {
+    
+     //Throws an error if the parameter is not an array or if the function is called without an array being passed
+     if(!Array.isArray(array) || arguments.length !== 1 ){
+        return para.textContent = 'You must input one array as an argument!'
     }
-
-//Throws an error if the edge case of the array having only one item
-if(array.length < 2){
-    return para.textContent = 'The array must have at least two numbers'
-    }
-
-//Declares a variable that holds a value of true if all items are positive numbers
-const areAllNumbers = array.every(item => typeof item === 'number' && item > 0);
-//Throws an error if each item of the array is not a positive number
-  if(!areAllNumbers){
-    return para.textContent = 'Each item has to be a positive number!'
-  }
-
-
-//designates the first item of the array to temporarily hold the value of the largest number so that each element in the array can be checked against this number 
-let largestNo = array[0];
-
-//loop that processes the array to find the largest number
+    
+    //Throws an error if the input is an empty array
+     if(array.length === 0){
+         return para.textContent = 'The array cannot be empty!'
+        }
+    
+    //Declares a variable that holds a value of true if all items are numbers
+    const areAllNumbers = array.every(item => typeof item === 'number');
+    
+    //Throws an error if each item of the array is not a number
+      if(!areAllNumbers){
+        return para.textContent = 'Each item has to be a number!'
+      }
+    
+    
+    //designates the first item of the array to temporarily hold the value of the largest number so that each element in the array can be checked against this number 
+    let largestNo = array[0];
+    
+    //loop that processes the array to find the largest number
     for(let i = 0; i < array.length; i++){
         //checks the current value against the current largest number
         if(array[i] > largestNo){
@@ -86,8 +87,10 @@ let largestNo = array[0];
             largestNo = array[i]
         }
     }
+    
     //populates the largest number into the paragraph
-     return para.textContent = largestNo;
-}
-
-findLargestNumber(numbers)
+         return para.textContent = largestNo;
+    }
+    
+    findLargestNumber(numbers)
+})();
